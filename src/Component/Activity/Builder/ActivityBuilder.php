b892c0d927df9da2170f56579bc59dfc88d7e8bc
Frank Bardon Jr <bardonf@upenn.edu>
2014-10-01 10:18:35 -0400

    Brough back changes lost from composer update from Prototype Component.

---
+++ src/Component/Activity/Builder/ActivityBuilder.php
@@ -0,0 +1,109 @@
+<?php
+
+/**
+ * This file is part of the Accard package.
+ *
+ * (c) University of Pennsylvania
+ *
+ * For the full copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+namespace Accard\Component\Activity\Builder;
+
+use Accard\Component\Resource\Builder\AbstractBuilder;
+use Accard\Component\Resource\Repository\RepositoryInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+/**
+ * Activity builder.
+ *
+ * Used to ease the programatic creation of activities.
+ *
+ * @author Frank Bardon Jr. <bardonf@upenn.edu>
+ */
+class ActivityBuilder extends AbstractBuilder implements ActivityBuilderInterface
+{
+    /**
+     * Activity repository.
+     *
+     * @var RepositoryInterface
+     */
+    protected $activityRepository;
+
+    /**
+     * Field repository.
+     *
+     * @var RepositoryInterface
+     */
+    protected $fieldRepository;
+
+    /**
+     * Field value repository.
+     *
+     * @var RepositoryInterface
+     */
+    protected $fieldValueRepository;
+
+
+    /**
+     * Constructor.
+     *
+     * @param ObjectManager $manager
+     * @param RepositoryInterface $activityRepository
+     */
+    public function __construct(ObjectManager $manager,
+                                RepositoryInterface $activityRepository,
+                                RepositoryInterface $fieldRepository,
+                                RepositoryInterface $fieldValueRepository)
+    {
+        $this->manager = $manager;
+        $this->activityRepository = $activityRepository;
+        $this->fieldRepository = $fieldRepository;
+        $this->fieldValueRepository = $fieldValueRepository;
+    }
+
+    public function getFieldRepository()
+    {
+        return $this->fieldRepository;
+    }
+
+    public function getFieldValueRepository()
+    {
+        return $this->fieldValueRepository;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function create()
+    {
+        $this->resource = $this->activityRepository->createNew();
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addField($name, $value, $presentation = null)
+    {
+        $field = $this->fieldRepository->findOneBy(array('name' => $name));
+
+        if (null === $field) {
+            $field = $this->fieldRepository->createNew();
+            $field->setName($name);
+            $field->setPresentation($presentation ?: $name);
+
+            $this->manager->persist($field);
+            $this->manager->flush($field);
+        }
+
+        $fieldValue = $this->fieldValueRepository->createNew();
+        $fieldValue->setField($field);
+        $fieldValue->setValue($value);
+
+        $this->resource->addField($fieldValue);
+
+        return $this;
+    }
+}
---
+++ src/Component/Activity/Builder/ActivityBuilderInterface.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file is part of the Accard package.
+ *
+ * (c) University of Pennsylvania
+ *
+ * For the full copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+namespace Accard\Component\Activity\Builder;
+
+use Accard\Component\Resource\Builder\BuilderInterface;
+
+/**
+ * Activity Builder.
+ *
+ * Used to ease the programmatic creation of activities.
+ *
+ * @author Frank Bardon Jr. <bardonf@upenn.edu>
+ */
+interface ActivityBuilderInterface extends BuilderInterface
+{
+    /**
+     * Create a new activity.
+     *
+     * @return ActivityBuilderInterface
+     */
+    public function create();
+
+    /**
+     * Add field with name and value.
+     *
+     * @param string $name
+     * @param mixed $value
+     * @param string|null $presentation
+     * @return ActivityBuilderInterface
+     */
+    public function addField($name, $value, $presentation = null);
+}
