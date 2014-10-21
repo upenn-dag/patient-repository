6e85b27d3ad23556898a8f3575388298f3ed124d
Frank Bardon Jr <bardonf@upenn.edu>
2014-10-21 14:13:18 -0400

    Refactored all field create and update views to use better inheritance.

--- src/Bundle/FieldBundle/Form/Type/FieldType.php
+++ src/Bundle/FieldBundle/Form/Type/FieldType.php
@@ -66,21 +66,21 @@ class FieldType extends AbstractType
     {
         $builder
             ->add('name', 'text', array(
-                'label' => 'accard.form.field.name'
+                'label' => 'accard.field.form.name'
             ))
             ->add('presentation', 'text', array(
-                'label' => 'accard.form.field.presentation'
+                'label' => 'accard.field.form.presentation'
             ))
             ->add('type', 'choice', array(
-                'label' => 'accard.form.field.type',
+                'label' => 'accard.field.form.type',
                 'choices' => FieldTypes::getChoices()
             ))
             ->add('option', 'accard_option_choice', array(
-                'label' => 'accard.form.field.option',
+                'label' => 'accard.field.form.option',
                 'required' => false
             ))
             ->add('allowMultiple', 'checkbox', array(
-                'label' => 'accard.form.field.allow_multiple',
+                'label' => 'accard.field.form.allow_multiple',
                 'required' => false,
             ))
         ;
