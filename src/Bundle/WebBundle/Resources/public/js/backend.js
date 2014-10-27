6a3c3282d699c880f065fea3ae3b46af173f3ce5
Frank Bardon Jr <bardonf@upenn.edu>
2014-10-27 14:18:14 -0400

    Fixed date picker javascript implementation.

--- src/Bundle/WebBundle/Resources/public/js/backend.js
+++ src/Bundle/WebBundle/Resources/public/js/backend.js
@@ -1,18 +1,24 @@
+// Init the side menu.
 $(function() {
-
     $('#side-menu').menu();
-
 });
 
-//Loads the correct sidebar on window load,
-//collapses the sidebar on window resize.
+// Loads the correct sidebar on window load,
+// collapses the sidebar on window resize.
 $(function() {
     $(window).bind("load resize", function() {
         width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
         if (width < 768) {
-            $('div.sidebar-collapse').addClass('collapse')
+            $('div.sidebar-collapse').addClass('collapse');
         } else {
-            $('div.sidebar-collapse').removeClass('collapse')
+            $('div.sidebar-collapse').removeClass('collapse');
         }
-    })
-})
+    });
+});
+
+// Initialize the bootstrap picker if required.
+$(function() {
+    if (!Modernizr.inputtypes.date) {
+        $('input[type=date]').datepicker();
+    }
+});
