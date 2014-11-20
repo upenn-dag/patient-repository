16225d956f3f8917cbef252a6e88a52ff2014c62
Frank Bardon Jr <bardonf@upenn.edu>
2014-11-20 13:34:28 -0500

    Fixed bootstrap datepicker initialization so it actually saves.

--- src/Bundle/WebBundle/Resources/public/js/backend.js
+++ src/Bundle/WebBundle/Resources/public/js/backend.js
@@ -1,11 +1,7 @@
-// Init the side menu.
-$(function() {
-    $('#side-menu').menu();
-});
-
 // Loads the correct sidebar on window load,
 // collapses the sidebar on window resize.
 $(function() {
+    $('#side-menu').menu();
     $(window).bind("load resize", function() {
         width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
         if (width < 768) {
@@ -19,6 +15,6 @@ $(function() {
 // Initialize the bootstrap picker if required.
 $(function() {
     if (!Modernizr.inputtypes.date) {
-        $('input[type=date]').datepicker();
+        $('input[type=date]').attr('type', 'text').datepicker({ format: 'yyyy-mm-dd' });
     }
 });
