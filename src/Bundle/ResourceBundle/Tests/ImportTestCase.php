9001d7470fc514bd0ad4d0288667276420f4f7a4
Dylan Pierce <piercedy@upenn.edu>
2015-02-19 11:46:05 -0500

    Added base import test suite.

---
+++ src/Bundle/ResourceBundle/Tests/ImportTestCase.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Accard\Bundle\ResourceBundle\Tests;
+
+use PHPUnit_Framework_TestCase;
+
+class ImportTestCase extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Placeholder test for import cases.
+     */
+    public function testPlaceholder()
+    {
+        $this->assertTrue(true);
+    }
+}
