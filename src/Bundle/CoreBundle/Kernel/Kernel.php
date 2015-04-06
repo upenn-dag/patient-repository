70cf569e0eca9957d5b182f93c05083a1d9cccdb
Frank Bardon Jr <bardonf@upenn.edu>
2015-04-06 11:46:50 -0400

    Disabled swiftmailer bundle.

--- src/Bundle/CoreBundle/Kernel/Kernel.php
+++ src/Bundle/CoreBundle/Kernel/Kernel.php
@@ -59,7 +59,7 @@ abstract class Kernel extends BaseKernel
             new \Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new \Symfony\Bundle\TwigBundle\TwigBundle(),
             new \Symfony\Bundle\MonologBundle\MonologBundle(),
-            new \Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            //new \Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new \Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new \Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new \Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
