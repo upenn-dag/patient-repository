14ab3daa8489109b9b052c1b882d8439d55a994f
Frank Bardon Jr <bardonf@upenn.edu>
2015-04-15 11:16:29 -0400

    Removed acceptance and functional test suites from activity component

--- src/Component/Activity/tests/unit/Model/ActivityTest.php
+++ src/Component/Activity/tests/unit/Model/ActivityTest.php
@@ -10,6 +10,8 @@
  */
 namespace Model;
 
+use DateTime;
+use Codeception\Specify;
 use Accard\Component\Activity\Model\Activity;
 use Accard\Component\Activity\Model\ActivityInterface;
 
@@ -20,14 +22,42 @@ use Accard\Component\Activity\Model\ActivityInterface;
  */
 class ActivityTest extends \Codeception\TestCase\Test
 {
-    use \Codeception\Specify;
+    use Specify;
 
-    public function testCreation()
+    public function _before()
     {
-        $activity = new Activity();
+        $this->activity = new Activity();
+    }
 
+    public function testActivityCreation()
+    {
         $this->specify('interface is followed', function() {
-            $this->assertTrue($activity instanceof ActivityInterface);
+            $this->assertInstanceOf(
+                'Accard\Component\Activity\Model\ActivityInterface',
+                $this->activity
+            );
+        });
+
+        $this->specify('id is unset on creation', function() {
+            $this->assertNull($this->activity->getId());
+        });
+
+        $this->specify('fields collection is initialized and empty on creation', function() {
+            $this->assertAttributeInstanceOf(
+                'Doctrine\Common\Collections\Collection',
+                'fields',
+                $this->activity
+            );
+            $this->assertAttributeCount(0, 'fields', $this->activity);
+            $this->assertCount(0, $this->activity->getFields());
+        });
+
+        $this->specify('date is mutable', function() {
+            $date = new DateTime();
+            $this->activity->setActivityDate($date);
+
+            $this->assertAttributeSame($date, 'activityDate', $this->activity);
+            $this->assertSame($date, $this->activity->getActivityDate());
         });
     }
 }
