d326f33cfedb39c991739735681f28426ee59cce
vasurenga <renganav@ACC-539PWN1.ACC.UPHS.UPENN.EDU>
2015-07-06 15:34:41 -0400

    Field Value test file commited

--- src/Component/Patient/Tests/unit/Model/FieldValueTest.php
+++ src/Component/Patient/Tests/unit/Model/FieldValueTest.php
@@ -2,8 +2,8 @@
 namespace AccardTest\Component\FieldValue\Model;
 
 use Mockery;
-use Accard\Component\Option\Model\FieldValue;
-use Accard\Component\Option\Model\FieldValueInterface;
+use Accard\Component\Patient\Model\FieldValue;
+use Accard\Component\Patient\Model\FieldValueInterface;
 
 class FieldValueTest extends \Codeception\TestCase\Test
 {
@@ -36,58 +36,10 @@ class FieldValueTest extends \Codeception\TestCase\Test
         );
     }
 
-    public function testFieldValueIsAccardResource()
-    {
-        $this->assertInstanceOf(
-            'Accard\Component\Resource\Model\ResourceInterface',
-            $this->fieldValue
-        );
-    }
-
-    public function testFieldValueIsResourceLockable()
-    {
-        $this->assertInstanceOf(
-            'Accard\Component\Resource\Model\LockableInterface',
-            $this->fieldValue
-        );
-    }
 
     public function testFieldValueIdIsUnsetOnCreation()
     {
         $this->assertNull($this->fieldValue->getId());
-    }
-
-    public function testFieldAssociationIsMutable()
-    {
-        $this->fieldValue->setField($this->field);
-        $this->assertAttributeSame($this->field, 'field', $this->fieldValue);
-        $this->assertSame($this->field, $this->fieldValue->getField());
-    }
-
-    public function testFieldValueValuePropertyIsMutable()
-    {
-        $this->fieldValue->setPatient('NAME');
-        $this->assertAttributeSame('NAME', 'name', $this->fieldValue);
-        $this->assertSame('NAME', $this->fieldValue->getPatient());
-    }
-
-    public function testFieldValueFluency()
-    {
-        $this->assertSame($this->fieldValue, $this->fieldValue->setPatient('VALUE'));
-    }
-
-    /**
-     * @expectedException BadMethodCallException
-     */
-    public function testFieldNameProxyThrowsIfOptionIsNotSet()
-    {
-        $this->fieldValue->getPatient();
-    }
-
-    public function testFieldValueStringConversionValueIsRepresented()
-    {
-        $this->assertInternalType('string', (string) $this->fieldValue);
-        $this->assertEquals('', (string) $this->fieldValue);
     }
 
 
