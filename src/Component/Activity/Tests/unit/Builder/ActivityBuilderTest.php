69fa7a6ce85f55eaa4dc0bf6c7de66981f7d78f2
Frank Bardon Jr <bardonf@upenn.edu>
2015-07-08 11:06:27 -0400

    [Activity] Full unit tests added for activity builder.

--- src/Component/Activity/Tests/unit/Builder/ActivityBuilderTest.php
+++ src/Component/Activity/Tests/unit/Builder/ActivityBuilderTest.php
@@ -1,88 +1,119 @@
 <?php
-namespace AccardTest\Component\Activity\Builder;
 
 /**
- * Attribut eBuilder Test
- * 
- * @author Dylan Pierce <piercedy@upenn.edu>
+ * This file is part of the Accard package.
+ *
+ * (c) University of Pennsylvania
+ *
+ * For the full copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
  */
-use Accard\Component\Activity\Builder\ActivityBuilder;
-use Mockery;
+namespace AccardTest\Component\Activity\Builder;
 
-class ActivityBuilderTest extends \Codeception\TestCase\Test
+use Mockery;
+use Codeception\TestCase\Test;
+use Accard\Component\Activity\Builder\ActivityBuilder;
+
+/**
+ * Activity builder tests.
+ *
+ * @author Frank Bardon Jr. <bardonf@upenn.edu>
+ */
+class ActivityBuilderTest extends Test
 {
     protected function _before()
     {
-        $this->objectManager = Mockery::mock('Doctrine\Common\Persistence\ObjectManager');
+        $this->activity = Mockery::mock('Accard\Component\Activity\Model\ActivityInterface');
+        $this->field = Mockery::mock('Accard\Component\Activity\Model\FieldInterface');
+        $this->fieldValue = Mockery::mock('Accard\Component\Activity\Model\FieldValueInterface');
+
+        $this->objectManager = Mockery::mock('Doctrine\Common\Persistence\ObjectManager')
+            ->shouldReceive('persist')->zeroOrMoreTimes()
+            ->shouldReceive('flush')->zeroOrMoreTimes()
+            ->getMock();
         $this->activityRepository = Mockery::mock('Accard\Component\Resource\Repository\RepositoryInterface');
         $this->fieldRepository = Mockery::mock('Accard\Component\Resource\Repository\RepositoryInterface');
         $this->fieldValueRepository = Mockery::mock('Accard\Component\Resource\Repository\RepositoryInterface');
 
         $this->builder = new ActivityBuilder($this->objectManager, $this->activityRepository, $this->fieldRepository, $this->fieldValueRepository);
-
     }
 
-    protected function _after()
-    {
-    }
-
-    public function testActivityBuilderGetFieldRepositoryReceivesCreateNew()
-    {
-        $this->activityRepository->shouldReceive('createNew');
-
-        $this->assertSame($this->builder, $this->builder->create());
-    }
-
-    public function testActivityBuilderReturnsFieldRepository()
+    public function testActivityBuilderCanReturnFieldRepository()
     {
         $this->assertSame($this->fieldRepository, $this->builder->getFieldRepository());
     }
 
-    public function testActivityBuilderReturnsFieldValueRepository()
+    public function testActivityBuilderCanReturnFieldValueRepository()
     {
         $this->assertSame($this->fieldValueRepository, $this->builder->getFieldValueRepository());
     }
 
-    public function testActivityBuilderAddsFieldAndFieldValue()
+    public function testActivityBuilderCanCreateNewActivityResource()
     {
-        $field = Mockery::mock('Accard\Component\Field\Model\FieldInterface');
-
-        $fieldValue = Mockery::mock('Accard\Component\Field\Model\FieldValueInterface');
-        $fieldValue->shouldReceive('setField');
-        $fieldValue->shouldReceive('setValue');
-
-        $resource = Mockery::mock();
-        $resource->shouldReceive('addField');
-        $this->builder->set($resource);
-
-        $this->fieldRepository->shouldReceive('findOneBy')->andReturn($field);
-        $this->fieldValueRepository->shouldReceive('createNew')->andReturn($fieldValue);
-
-        $this->assertSame($this->builder, $this->builder->addField('NAME', 'VALUE'));
+        $this->enableActivityCreate();
+        $this->builder->create();
+        $this->assertAttributeSame($this->activity, 'resource', $this->builder);
     }
 
-    public function testActivityBuilderAddFieldCreatesNewFieldWhenFieldRepositoryReturnsNull()
+    public function testActivityBuilderCreateIsFluent()
     {
-        $resource = Mockery::mock();
-        $resource->shouldReceive('addField');
-        $this->builder->set($resource);
+        $this->enableActivityCreate();
+        $this->assertSame($this->builder->create(), $this->builder);
+    }
 
-        $this->fieldRepository->shouldReceive('findOneBy')->andReturn(null);
+    public function testActivityBuilderCanAddFieldWithoutPresentationWithoutDatabaseRecord()
+    {
+        $this->setupFieldAdditionScenario(false, true);
+        $this->builder->addField('NAME', 'VALUE', null);
+    }
 
-        $field = Mockery::mock('Accard\Component\Field\Model\FieldInterface');
+    public function testActivityBuilderCanAddFieldWithoutPresentationWithDatabaseRecord()
+    {
+        $this->setupFieldAdditionScenario(true, true);
+        $this->builder->addField('NAME', 'VALUE', null);
+    }
 
-        $this->fieldRepository->shouldReceive('createNew')->andReturn($field);
-        $field->shouldReceive('setName')->with('NAME');
-        $field->shouldReceive('setPresentation')->with('PRESENTATION');
+    public function testActivityBuilderCanAddFieldWithPresentationWithoutDatabaseRecord()
+    {
+        $this->setupFieldAdditionScenario(false, false);
+        $this->builder->addField('NAME', 'VALUE', 'PRESENTATION');
+    }
 
-        $this->objectManager->shouldReceive('persist')->with($field);
-        $this->objectManager->shouldReceive('flush')->with($field);
+    public function testActivityBuilderAddFieldIsFluent()
+    {
+        $this->setupFieldAdditionScenario(true, true);
+        $this->assertSame($this->builder, $this->builder->addField('NAME', 'VALUE', null));
+    }
 
-        $fieldValue = Mockery::mock('Accard\Component\Field\Model\FieldValueInterface');
-        $fieldValue->shouldReceive('setField');
-        $fieldValue->shouldReceive('setValue');
-        $this->fieldValueRepository->shouldReceive('createNew')->andReturn($fieldValue);
 
-        $this->assertSame($this->builder, $this->builder->addField('NAME', 'VALUE', 'PRESENTATION'));
+    // Privates
+
+    private function enableActivityCreate()
+    {
+        $this->activityRepository->shouldReceive('createNew')->once()->andReturn($this->activity);
+    }
+
+    private function setupFieldAdditionScenario($findInDb, $presentationIsNull)
+    {
+        $this->enableActivityCreate();
+        $this->builder->create();
+
+        $this->activity->shouldReceive('addField')->zeroOrMoreTimes()->andReturn(Mockery::self());
+        $this->fieldValueRepository->shouldReceive('createNew')->once()->andReturn($this->fieldValue);
+        $this->fieldValue->shouldReceive('setField')->once()->with($this->field)->andReturn(Mockery::self());
+
+        if ($findInDb) {
+            $this->fieldRepository->shouldReceive('findOneBy')->once()->andReturn($this->field);
+        } else {
+            $this->fieldRepository->shouldReceive('findOneBy')->once()->andReturn(null);
+            $this->fieldRepository->shouldReceive('createNew')->once()->andReturn($this->field);
+            $this->field->shouldReceive('setName')->once()->with('NAME')->andReturn(Mockery::self());
+            if ($presentationIsNull) {
+                $this->field->shouldReceive('setPresentation')->once()->with('NAME')->andReturn(Mockery::self());
+            } else {
+                $this->field->shouldReceive('setPresentation')->once()->with('PRESENTATION')->andReturn(Mockery::self());
+            }
+
+        }
     }
 }
