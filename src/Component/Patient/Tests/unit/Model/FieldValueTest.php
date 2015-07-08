e0afbd743821f8e59edd30ea6cdfbad1f88dbb4d
Frank Bardon Jr <bardonf@upenn.edu>
2015-07-08 09:46:04 -0400

    [Patient] Full unit tested field value model.

--- src/Component/Patient/Tests/unit/Model/FieldValueTest.php
+++ src/Component/Patient/Tests/unit/Model/FieldValueTest.php
@@ -1,27 +1,26 @@
 <?php
+
+/**
+ * This file is part of the Accard package.
+ *
+ * (c) University of Pennsylvania
+ *
+ * For the full copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
 namespace AccardTest\Component\FieldValue\Model;
 
 use Mockery;
+use Codeception\TestCase\Test;
 use Accard\Component\Patient\Model\FieldValue;
 use Accard\Component\Patient\Model\FieldValueInterface;
 
-class FieldValueTest extends \Codeception\TestCase\Test
+class FieldValueTest extends Test
 {
-    /**
-     * @var \UnitTester
-     */
-    protected $tester;
-
     protected function _before()
     {
-        $this->field = Mockery::mock('Accard\Component\Patient\Model\FieldValueInterface');
-        $this->field->shouldReceive('getSubject')->zeroOrMoreTimes()->andReturn('NAME');
-       
+        $this->patient = Mockery::mock('Accard\Component\Patient\Model\Patient');
         $this->fieldValue = new FieldValue();
-    }
-
-    protected function _after()
-    {
     }
 
     /**
@@ -29,19 +28,31 @@ class FieldValueTest extends \Codeception\TestCase\Test
      */
     public function testFieldValueInterfaceIsFollowed()
     {
-
         $this->assertInstanceOf(
             'Accard\Component\Patient\Model\FieldValueInterface',
             $this->fieldValue
         );
     }
 
-
     public function testFieldValueIdIsUnsetOnCreation()
     {
         $this->assertNull($this->fieldValue->getId());
     }
 
+    public function testFieldValueSubjectIsNullOnCreation()
+    {
+        $this->assertAttributeSame(null, 'subject', $this->fieldValue);
+    }
 
+    public function testFieldValueHasPatientAliasForSettingSubject()
+    {
+        $this->fieldValue->setPatient($this->patient);
+        $this->assertAttributeSame($this->patient, 'subject', $this->fieldValue);
+    }
 
+    public function testFieldValueHasPatientAliasForGettingSubject()
+    {
+        $this->fieldValue->setPatient($this->patient);
+        $this->assertSame($this->patient, $this->fieldValue->getPatient());
+    }
 }
