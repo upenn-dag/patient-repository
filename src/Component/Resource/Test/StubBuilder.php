cd29acaa41266e43ec0e6f23a0181457cb394b50
Frank Bardon Jr <bardonf@upenn.edu>
2015-07-08 13:05:27 -0400

    [Resource] Created stub test for testing the abstract resource builder.

---
+++ src/Component/Resource/Test/StubBuilder.php
@@ -0,0 +1,25 @@
+<?php
+
+
+/**
+ * This file is part of the Accard package.
+ *
+ * (c) University of Pennsylvania
+ *
+ * For the full copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+namespace Accard\Component\Resource\Test;
+
+use Accard\Component\Resource\Builder\AbstractBuilder;
+
+/**
+ * Stub builder.
+ *
+ * Serves as an implementation of the abstract builder for test purposes.
+ *
+ * @author Frank Bardon Jr. <bardonf@upenn.edu>
+ */
+class StubBuilder extends AbstractBuilder
+{
+}
