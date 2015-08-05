dac2d90eed276bd17a0b42b27350a16f5a40b37d
Frank Bardon Jr <bardonf@upenn.edu>
2015-08-05 08:38:19 -0400

    [Subsplit] Now allows --components --bridges and --bundles options so we can limit our export time.

--- accard-subtree.sh
+++ accard-subtree.sh
@@ -1,13 +1,47 @@
 #!/bin/bash
 
 # Ensure git subsplit is installed.
-if [ ! type "git subsplit" > /dev/null ]; then
+if [ ! type "git subsplit" >/dev/null 2>&1 ]; then
 
     echo "To run this command, you must first install git subsplit."
     echo "Visit: https://github.com/dflydev/git-subsplit"
     exit 1
 
 fi
+
+COMPONENTS=false
+BUNDLES=false
+BRIDGES=false
+FULL=false
+
+# Parse incoming arguments
+while [[ $# > 0 ]]
+do
+key="$1"
+case $key in
+    --components)
+    COMPONENTS=true
+    shift
+    ;;
+    --bundles)
+    BUNDLES=true
+    shift
+    ;;
+    --bridges)
+    BRIDGES=true
+    shift
+    ;;
+    --full)
+    FULL=true
+    shift
+    ;;
+esac
+shift
+done
+
+echo $COMPONENTS
+echo $BUNDLES
+echo $BRIDGES
 
 components=("Activity" "Attribute" "Behavior" "Core" "Diagnosis" "Drug" "Patient" "Phase" "Regimen" "Sample")
 bundles=("ActivityBundle" "AttributeBundle" "BehaviorBundle" "CoreBundle" "CPDBundle" "DiagnosisBundle" "DrugBundle" "HMTBBundle" "OutcomesBundle" "PDSBundle" "PatientBundle" "PhaseBundle" "RegimenBundle" "SampleBundle" "TemplateBundle" "WebBundle")
@@ -22,64 +56,26 @@ bridges=("Twig")
 git subsplit init git@gitlab.med.upenn.edu:prototype/PatientRepository.git
 git subsplit update
 
-for component in "${components[@]}"
-do
-    echo "Splitting ${component} component"
-    git subsplit publish "src/Component/${component}:git@gitlab.med.upenn.edu:prototype/${component}.git" --heads="master 1.0"
-    sleep 1 # Added for coherence of output
-done
+if [ "$COMPONENTS" = true ]; then
+    for component in "${components[@]}"
+    do
+        echo "Splitting ${component} component"
+        git subsplit publish "src/Component/${component}:git@gitlab.med.upenn.edu:prototype/${component}.git" --heads="master 1.0"
+    done
+fi
 
-for bundle in "${bundles[@]}"
-do
-    echo "Splitting ${bundle} bundle"
-    git subsplit publish "src/Bundle/${bundle}:git@gitlab.med.upenn.edu:prototype/${bundle}.git" --heads="master 1.0"
-    sleep 1 # Added for coherence of output
-done
+if [ "$BUNDLES" = true ]; then
+    for bundle in "${bundles[@]}"
+    do
+        echo "Splitting ${bundle} bundle"
+        git subsplit publish "src/Bundle/${bundle}:git@gitlab.med.upenn.edu:prototype/${bundle}.git" --heads="master 1.0"
+    done
+fi
 
-for bridge in "${bridges[@]}"
-do
-    echo "Splitting ${bridge}"
-    git subsplit publish "src/Bridge/${bridge}:git@gitlab.med.upenn.edu:prototype/${bridge}.git" --heads="master 1.0"
-    sleep 1 # Added for coherence of output
-done
-
-## This is the listing of currently running splits.
-
-#  Activity
-#  Attribute
-#  Behavior
-#  Core
-#  Diagnosis
-#  Drug
-#  Field
-#  Option
-#  Patient
-#  Phase
-#  Prototype
-#  Regimen
-#  Resource
-#  Sample#  
-
-#  ActivityBundle
-#  AttributeBundle
-#  BehaviorBundle
-#  CPDBundle
-#  CoreBundle
-#  DiagnosisBundle
-#  DrugBundle
-#  FieldBundle
-#  HMTBBundle
-#  OptionBundle
-#  OutcomesBundle
-#  PDSBundle
-#  PatientBundle
-#  PhaseBundle
-#  PrototypeBundle
-#  RegimenBundle
-#  ResourceBundle
-#  SampleBundle
-#  SettingsBundle
-#  TemplateBundle
-#  WebBundle#  
-
-#  Twig
+if [ "$BRIDGES" = true ]; then
+    for bridge in "${bridges[@]}"
+    do
+        echo "Splitting ${bridge}"
+        git subsplit publish "src/Bridge/${bridge}:git@gitlab.med.upenn.edu:prototype/${bridge}.git" --heads="master 1.0"
+    done
+fi
